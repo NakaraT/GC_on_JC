f19291b7415c66dbd401d34d8031574e7b474a8f
NakaraT <slava.tarakanov@mail.ru>
2023-10-29 23:33:14 +0300

    \Almost working/

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.ui.components.DashboardScreen
+import com.example.myapplication.ui.components.HomeScreen
+import com.example.myapplication.ui.components.InformationScreen
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.utils.Constants
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // remember navController so it does not
+                // get recreated on recomposition
+                val navController = rememberNavController()
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    color = MaterialTheme.colorScheme.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigationBar(navController = navController)
+                        }, content = { padding ->
+                            // Navhost: where screens are placed
+                            NavHostContainer(navController = navController, padding = padding)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun NavHostContainer(
+    navController: NavHostController,
+    padding: PaddingValues
+) {
+
+    NavHost(
+        navController = navController,
+
+        // set the start destination as home
+        startDestination = "home",
+
+        // Set the padding provided by scaffold
+        modifier = Modifier.padding(paddingValues = padding),
+
+        builder = {
+
+            // route : Home
+            composable("home") {
+                HomeScreen()
+            }
+
+            // route : information
+            composable("information") {
+                InformationScreen()
+            }
+
+            // route : information
+            composable("dashboard") {
+                DashboardScreen()
+            }
+        }
+    )
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    BottomNavigation(
+        // set background color
+        backgroundColor = Color(0xFFe5d4f9)) {
+        // observe the backstack
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        // observe current route to change the icon
+        // color,label color when navigated
+        val currentRoute = navBackStackEntry?.destination?.route
+        // Bottom nav items we declared
+        Constants.BottomNavItems.forEach { navItem ->
+            // Place the bottom nav items
+            BottomNavigationItem(
+                // it currentRoute is equal then its selected route
+                selected = currentRoute == navItem.route,
+                // navigate on click
+                onClick = {
+                    navController.navigate(navItem.route)
+                },
+                // Icon of navItem
+                icon = {
+                    Icon(imageVector = navItem.icon, contentDescription = navItem.label)
+                },
+                // label
+                label = {
+                    Text(text = navItem.label)
+                },
+                alwaysShowLabel = false
+            )
+        }
+    }
+}
